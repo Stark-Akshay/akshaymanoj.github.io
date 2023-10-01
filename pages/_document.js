a4a5716ebc473b9d54c613b0ea7eb49f08f4dafe
Stark-Akshay <akshaymanoj.dms@gmail.com>
2023-10-01 02:35:06 +0100

    title

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Akshay&apos;s Portfolio</title>
       <body>
         <Main />
         <NextScript />

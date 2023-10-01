ecdfbb1f69294f284d6f392262e5828d84cd1e3a
Stark-Akshay <akshaymanoj.dms@gmail.com>
2023-10-01 02:29:21 +0100

    build check

--- pages/components/Me.jsx
+++ pages/components/Me.jsx
@@ -7,7 +7,7 @@ const Me = () => {
     <>
     <section className='flex sm:h-[50vh] md:h-[50vh] lg:h-[60vh] justify-between flex-row flex-wrap'>
         <div className='p-10 flex flex-col justify-around'>
-          <p className='text-ascent text-opacity-50 min-[320px]:text-2xl sm:text-5xl md:text-5xl lg:text-5xl'>Hi I'm a</p>
+          <p className='text-ascent text-opacity-50 min-[320px]:text-2xl sm:text-5xl md:text-5xl lg:text-5xl'>Hi I am a</p>
           <p className='text-text min-[320px]:text-4xl md:text-[6rem] sm:text-[6rem] lg:text-[6rem]'>Front-end</p>
           <p className='text-text min-[320px]:text-4xl md:text-[6rem] sm:text-[6rem] lg:text-[6rem]'>Web Developer</p>
 

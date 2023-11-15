ef4d04b8292570999a54d0737b9143ce35cfb608
Stark-Akshay <akshaymanoj.dms@gmail.com>
2023-11-15 19:45:47 +0000

    project links added to the buttons

--- pages/components/Card.jsx
+++ pages/components/Card.jsx
@@ -8,8 +8,10 @@ const Card = (props) => {
       <h2 className="card-title">{props.title}</h2>
       <p>{props.desc1}</p>
       <p>{props.desc2}</p>
-      {props.code && <a className='w-full' href="https://github.com/Stark-Akshay/weatherapp"><button className='bg-ascent p-3 w-full rounded-lg flex items-center justify-center'>View the project</button></a>}
-      
+      {props.code && <a className='w-full' href={props.project}><button className='bg-ascent p-3 w-full rounded-lg flex items-center justify-center'>View the project</button></a>}
+
+      {props.code && <a className='w-full' href={props.code}><button className=' border-2 border-ascent p-3 w-full rounded-lg flex items-center justify-center'>View the code</button></a>}
+
     </div>
   </div>
   )
